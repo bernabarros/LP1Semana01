fd701cc4e1c60632a5e295d42d4577c89e3f8057
bernabarros <bernardobarros.work@gmail.com>
2025-02-20 10:32:49 +0000

    Added int variables and print them

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,7 +6,15 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            int inteiro = 1;
+            uint uinteiro = 2U;
+            long longo = 3L;
+            ulong ulongo = 4UL;
+
+            Console.WriteLine(inteiro);
+            Console.WriteLine(uinteiro);
+            Console.WriteLine(longo);
+            Console.WriteLine(ulongo);
         }
     }
 }
