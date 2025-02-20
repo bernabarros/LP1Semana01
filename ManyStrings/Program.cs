d9e08e5985b13caae82a30e68da35cbdf3d5a67b
bernabarros <bernardobarros.work@gmail.com>
2025-02-20 13:31:48 +0000

    Added string variables and print them

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ManyStrings
 {
@@ -6,7 +7,34 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            string teste = "Isto é um teste, \n com nova linha.";
+            string aspas = "Isto é um teste, com \"aspas\".";
+            string tabulacao = "Isto é um teste, com \t tabulação";
+            string zero = "Isto é um teste, com nulo \0 .";
+
+            string parenteses ="Isto é um teste, com \u0028parenteses\u0029"
+            ;
+            string soma = "Isto é um teste, com soma \u0031 \u002B " +
+            "\u0031 \u003D \u0032.";
+            string nome = "Isto é um teste, com a palavra \u004E\u004f"+
+            "\u004D\u0045.";
+
+            string caracter_unicode ="Isto é um teste, \n"+
+            "com caracteres e unicode\u002E";
+
             Console.WriteLine("Hello LP1!");
+            Console.WriteLine(teste);
+            Console.WriteLine(aspas);
+            Console.WriteLine(tabulacao);
+            Console.WriteLine(zero);
+
+            Console.WriteLine(parenteses);
+            Console.WriteLine(soma);
+            Console.WriteLine(nome);
+            Console.WriteLine(caracter_unicode);
+
+
         }
     }
 }
