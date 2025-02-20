e0c278155b4e07dbcab6c23d53fb091aeeabb2a4
bernabarros <bernardobarros.work@gmail.com>
2025-02-20 10:48:24 +0000

    Added char variables and print them

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace VariousTypes
 {
@@ -6,15 +7,29 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
             int inteiro = 1;
             uint uinteiro = 2U;
             long longo = 3L;
             ulong ulongo = 4UL;
 
+            char letra_a = '\u0041';
+            char exclamacao = '\u0021';
+            char numero_nove = '\u0039';
+            char sinal_igual = '\u003D';
+
             Console.WriteLine(inteiro);
             Console.WriteLine(uinteiro);
             Console.WriteLine(longo);
             Console.WriteLine(ulongo);
+
+            Console.WriteLine(letra_a);
+            Console.WriteLine(exclamacao);
+            Console.WriteLine(numero_nove);
+            Console.WriteLine(sinal_igual);
+
+
         }
     }
 }
